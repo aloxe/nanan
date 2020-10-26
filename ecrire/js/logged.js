e39d195d4a8a0b8e5b769de526132262de8c6d56
Alix Guillard <github@blog.re>
2020-10-27 00:29:32 +0100

    keep modal in sight

--- ecrire/js/logged.js
+++ ecrire/js/logged.js
@@ -4,4 +4,17 @@ $(document).ready(function(){
         handle: ".modal-header"
     });
     setEditor();
+
+    $(window).on('scroll', function () {
+        var scrollTop = $(window).scrollTop(),
+            upOffset = ($('#dialog').offset().top - scrollTop);
+        if (upOffset < 0)
+            $('#dialog').css("top", scrollTop);
+
+        var downOffset = scrollTop + $(window).height() - $('#dialog').offset().top,
+            heightBox = $('#dialog').height(),
+            scrollDown = scrollTop + $(window).height() - heightBox;
+        if (downOffset < heightBox)
+            $('#dialog').css("top", scrollDown);
+    });
 });
